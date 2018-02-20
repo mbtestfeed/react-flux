98ab04bd574ee131bccaeac3e5ede8a97b606d94
mbtestfeed <michael.ball@homes.co.nz>
2018-02-21 10:33:24 +1300

    Initial commit. Base project running.

---
+++ src/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var React = require('react');
+var ReactDom = require('react-dom');
+var ReactRouter = require('react-router-dom');
+// var App = require('./components/app');
+
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+// var InitializeActions = require('./actions/initializeActions');
+//
+// InitializeActions.initApp();
+
+ReactDom.render(
+    <Router>
+        {/* <Route path="/" component={App} /> */}
+        <p>Hi there</p>
+    </Router>, document.getElementById('app')
+);
